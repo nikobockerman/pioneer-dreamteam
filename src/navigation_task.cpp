6f19a352f6c14a26995716a34fd6ffd86a28b0c6
Niko Böckerman <niko.bockerman@aalto.fi>
2012-11-13 19:01:49 +0200

    Working navigation code for simulator

--- src/navigation_task.cpp
+++ src/navigation_task.cpp
@@ -4,6 +4,8 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+void prepareGoalList(std::vector<move_base_msgs::MoveBaseGoal>& goalList);
+
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
 
@@ -15,24 +17,101 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  move_base_msgs::MoveBaseGoal goal;
+  std::vector<move_base_msgs::MoveBaseGoal> goalList;
+  prepareGoalList(goalList);
+  
+  
+  for (std::vector<move_base_msgs::MoveBaseGoal>::iterator it = goalList.begin(); it != goalList.end(); ++it)
+  {
+    it->target_pose.header.stamp = ros::Time::now();
 
-  //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "base_link";
-  goal.target_pose.header.stamp = ros::Time::now();
+    ROS_INFO("Sending goal");
+    ac.sendGoal(*it);
 
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+    ac.waitForResult();
 
-  ROS_INFO("Sending goal");
-  ac.sendGoal(goal);
-
-  ac.waitForResult();
-
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-  else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO("Hooray, the base moved 1 meter forward");
+    else
+      ROS_INFO("The base failed to move forward 1 meter for some reason");
+  }
 
   return 0;
+}
+
+
+void prepareGoalList(std::vector<move_base_msgs::MoveBaseGoal>& goalList)
+{
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = 8.6;
+    goal.target_pose.pose.position.y = 1.13;
+    goal.target_pose.pose.orientation.z = -0.3;
+    goal.target_pose.pose.orientation.w = 1.0;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = 5.7;
+    goal.target_pose.pose.position.y = 2.8;
+    goal.target_pose.pose.orientation.z = 1.0;
+    goal.target_pose.pose.orientation.w = 0.3;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = 1.3;
+    goal.target_pose.pose.position.y = 7.6;
+    goal.target_pose.pose.orientation.z = 0.2;
+    goal.target_pose.pose.orientation.w = 1.0;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = 8.6;
+    goal.target_pose.pose.position.y = 7.5;
+    goal.target_pose.pose.orientation.z = 1.0;
+    goal.target_pose.pose.orientation.w = -0.3;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = 3.1;
+    goal.target_pose.pose.position.y = 12.3;
+    goal.target_pose.pose.orientation.z = 1.0;
+    goal.target_pose.pose.orientation.w = 0.0;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = -4.0;
+    goal.target_pose.pose.position.y = 9.9;
+    goal.target_pose.pose.orientation.z = -0.7;
+    goal.target_pose.pose.orientation.w = 0.7;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = -3.9;
+    goal.target_pose.pose.position.y = 0.8;
+    goal.target_pose.pose.orientation.z = -0.2;
+    goal.target_pose.pose.orientation.w = 1.0;
+    goalList.push_back(goal);
+  }
+  {
+    move_base_msgs::MoveBaseGoal goal;
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.pose.position.x = 0.3;
+    goal.target_pose.pose.position.y = 0.0;
+    goal.target_pose.pose.orientation.z = 0.0;
+    goal.target_pose.pose.orientation.w = 1.0;
+    goalList.push_back(goal);
+  }
 }
