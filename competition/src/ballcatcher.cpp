d48c832b7e54b0a7adf26b5fb454dd4c873ce8fc
Niko Böckerman <niko.bockerman@aalto.fi>
2012-12-12 09:54:21 +0200

    Improvements to navigation tasks
    Hopefully driving close to ball and to base
    now succeed. They should unless the robot gets stuck

--- competition/src/ballcatcher.cpp
+++ competition/src/ballcatcher.cpp
@@ -5,6 +5,7 @@
 #include <move_base_msgs/MoveBaseGoal.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <tf/transform_listener.h>
+#include <angles/angles.h>
 
 #include "common/robotstate.h"
 #include "common/functions.h"
@@ -22,11 +23,11 @@ private:
   void requestStateChange(const robotstate::State& requestedState);
   
   bool driveManipulator(int command);
-  void approachBall(const bool changedState);
+  void approachBall();
   void driveToBall();
   void driveToBase();
   move_base_msgs::MoveBaseGoal findPointToBase();
-  move_base_msgs::MoveBaseGoal findPointToBall();
+  move_base_msgs::MoveBaseGoal findPointToBall(bool firstFailed);
   competition::Ball findClosestRedBall(const geometry_msgs::Pose& currentPosition);
   
   ros::NodeHandle nh_;
@@ -37,6 +38,8 @@ private:
   ros::ServiceClient redBallsClient_;
   ros::ServiceClient moveSrvClient_;
   actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> moveClient_;
+  
+  std::default_random_engine re_;
 };
 
 
@@ -72,9 +75,10 @@ void PickupStateMachine::stateChangeHandler(const robotstate::State& oldState)
   {
     case robotstate::Approach:
       if (oldState != robotstate::Approach)
-        approachBall(true);
-      else
-        approachBall(false);
+      {
+        approachBall();
+        requestStateChange(robotstate::Centering);
+      }
       break;
     case robotstate::Centering:
       if (oldState != robotstate::Centering)
@@ -196,7 +200,7 @@ competition::Ball PickupStateMachine::findClosestRedBall(const geometry_msgs::Po
 
 
 
-move_base_msgs::MoveBaseGoal PickupStateMachine::findPointToBall()
+move_base_msgs::MoveBaseGoal PickupStateMachine::findPointToBall(bool firstFailed)
 {
   // TODO specify this distance
   const double distanceToBall = 0.40; // Distance to origo from the goal point i.e., distance from base_link to gripper
@@ -210,23 +214,27 @@ move_base_msgs::MoveBaseGoal PickupStateMachine::findPointToBall()
   move_base_msgs::MoveBaseGoal tempGoal;
   
   bool foundPoint {false};
-  bool triedStraight {false};
+  bool triedStraight {firstFailed};
   while (not foundPoint)
   {
-    tempGoal = move_base_msgs::MoveBaseGoal();
+    move_base_msgs::MoveBaseGoal tempGoal{};
     tempGoal.target_pose.header.frame_id = "map";
     tempGoal.target_pose.header.stamp = ros::Time::now();
     
-    geometry_msgs::Point goalPoint;
-    double yaw {0.0};
+    nav_msgs::GetPlan plan;
+    plan.request.start = currentPosition;
+    plan.request.tolerance = 0.01;
+    
+    geometry_msgs::Point goalPoint1, goalPoint2;
+    
+    double x1 = currentPosition.pose.position.x;
+    double y1 = currentPosition.pose.position.y;
+    double x2 = closest.location.x;
+    double y2 = closest.location.y;
+    
     if (not triedStraight)
     {
       // Try point straight from robot to point
-      geometry_msgs::Point goalPoint1, goalPoint2;
-      double x1 = currentPosition.pose.position.x;
-      double y1 = currentPosition.pose.position.y;
-      double x2 = closest.location.x;
-      double y2 = closest.location.y;
       double xDiff = x2 - x1;
       double yDiff = y2 - y1;
       double k = yDiff / xDiff;
@@ -239,38 +247,51 @@ move_base_msgs::MoveBaseGoal PickupStateMachine::findPointToBall()
       goalPoint1.y = k * goalPoint1.x;
       goalPoint2.y = k * goalPoint2.x;
       
-      if (competition::distanceBetweenPoints(currentPosition.pose.position, goalPoint1) < competition::distanceBetweenPoints(currentPosition.pose.position, goalPoint2))
-        goalPoint = goalPoint1;
-      else
-        goalPoint = goalPoint2;
+      if (competition::distanceBetweenPoints(currentPosition.pose.position, goalPoint1) > competition::distanceBetweenPoints(currentPosition.pose.position, goalPoint2))
+        goalPoint1 = goalPoint2;
+    }
+    else
+    {
+      // Point with random angle.
+      std::uniform_int_distribution<unsigned int> randomAngle {1,360};
+      unsigned int angleDeg = randomAngle(re_);
+      double angle = angles::from_degrees(angleDeg);
       
-      yaw = atan2(goalPoint.y - y2, goalPoint.x - x2);
-      
+      double xdiff = sqrt(distanceToBall * distanceToBall / (1 + tan(angle) * tan(angle))); // + or - -> two points
+      goalPoint1.x = x2 + xdiff;
+      goalPoint2.x = x2 - xdiff;
+      goalPoint1.y = tan(angle) * (goalPoint1.x - x2) + y2;
+      goalPoint2.y = tan(angle) * (goalPoint2.x - x2) + y2;
+    }
+    
+    unsigned int loops {2};
+    if (not triedStraight)
+    {
+      loops = 1;
       triedStraight = true;
     }
-    else
+    
+    for (unsigned int loop = 0; loop < loops and not foundPoint; ++loop)
     {
-      // TODO Find another possible point along the circle around the ball.
-      ROS_ERROR("TODO Find another possible point along the circle around the ball.");
-    }
+      geometry_msgs::Point goalPoint = goalPoint1;
+      if (loop == 1)
+        goalPoint = goalPoint2;
+      
+      tempGoal.target_pose.pose.position = goalPoint;
+      double yaw = atan2(goalPoint.y - y2, goalPoint.x - x2);
+      tempGoal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
+      plan.request.goal = tempGoal.target_pose;
     
-    tempGoal.target_pose.pose.position = goalPoint;
-    tempGoal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
-    
-    nav_msgs::GetPlan plan;
-    plan.request.start = currentPosition;
-    plan.request.goal = tempGoal.target_pose;
-    plan.request.tolerance = 0.01;
-    
-    if (not moveSrvClient_.call(plan)) // Test if navigation can make a plan to the point
-    {      
-      ROS_ERROR("Path not found");
-    }
-    else
-    {
-      // TODO check that poses.size() > 0 if required. Test that with explore.
-      goal = tempGoal;
-      foundPoint = true;
+      if (not moveSrvClient_.call(plan)) // Test if navigation can make a plan to the point
+      {      
+        ROS_ERROR("Path not found");
+      }
+      else
+      {
+        // TODO check that poses.size() > 0 if required. Test that with explore.
+        goal = tempGoal;
+        foundPoint = true;
+      }
     }
   }
   
@@ -280,30 +301,48 @@ move_base_msgs::MoveBaseGoal PickupStateMachine::findPointToBall()
 
 void PickupStateMachine::driveToBase()
 {
-  move_base_msgs::MoveBaseGoal goal = findPointToBase();
-  
-  actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> moveClient(nh_, "move_base");
-  while(!moveClient.waitForServer(ros::Duration(2.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
+  bool succeeded {false};
+  while (not succeeded)
+  {
+    move_base_msgs::MoveBaseGoal goal = findPointToBase();
+    
+    actionlib::SimpleClientGoalState resultState = moveClient_.sendGoalAndWait(goal);
+    ROS_INFO("Result of navigation: %s", resultState.toString().c_str()); 
+    
+    if (resultState == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Navigation finished successfully. Hooray!");
+      succeeded = true;
+    }
+    else
+    {
+      ROS_INFO("Navigation failed to reach ball.");
+    }
   }
-  actionlib::SimpleClientGoalState resultState = moveClient.sendGoalAndWait(goal);
-  ROS_INFO("Result of navigation: %s", resultState.toString().c_str()); 
 }
 
 
-void PickupStateMachine::approachBall(const bool changedState)
+void PickupStateMachine::approachBall()
 {
-  
-  if (changedState)
+  bool succeeded {false};
+  bool firstFailed {false};
+  while (not succeeded)
   {
-    // TODO Find the approach point.
-    // send that point to navigation
-    // Start timer for checking the status
-  }
-  else
-  {
-    // TODO Check whether we have reached the point.
-    // When reached, stop timer, change state to Centering.
+    move_base_msgs::MoveBaseGoal goal = findPointToBall(firstFailed);
+    
+    actionlib::SimpleClientGoalState resultState = moveClient_.sendGoalAndWait(goal);
+    ROS_INFO("Result of navigation to ball: %s", resultState.toString().c_str());
+    
+    if (resultState == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Navigation finished successfully. Hooray!");
+      succeeded = true;
+    }
+    else
+    {
+      ROS_INFO("Navigation failed to reach ball.");
+      firstFailed = true;
+    }
   }
 }
 
